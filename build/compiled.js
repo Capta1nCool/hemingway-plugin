0d17274d43258ceb4f29bb7e36b98e27ea95e0aa
Captain Cool <coder0807@gmail.com>
2024-06-21 20:54:36 +0530

    feat. process_html func

--- build/compiled.js
+++ build/compiled.js
@@ -1,23 +1,38 @@
 (() => {
+  // lib/modules/process_html.js
+  function processHtml(html) {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, "text/html");
+    const plainText = doc.body.textContent || "";
+    const paragraphs = doc.getElementsByTagName("p");
+    return { plainText, paragraphs };
+  }
+
   // lib/plugin.js
   var plugin = {
-    constants: {},
     noteOption: {
       "Analyze": {
         check: async function(app, noteUUID) {
           const noteContent = await app.getNoteContent({ uuid: noteUUID });
           await this._loadScript("https://cdnjs.cloudflare.com/ajax/libs/marked/13.0.0/marked.min.js");
           const html = marked.parse(noteContent);
-          const parser = new DOMParser();
-          const doc = parser.parseFromString(html, "text/html");
-          console.log(doc.body.textContent);
-          return doc.body.textContent || "";
+          this.data = processHtml(html);
+          return true;
         },
         run: async function(app, noteUUID) {
           await app.alert("Hemingway analyzer");
           console.debug("Special message to the DevTools console");
         }
       }
+    },
+    calculateLevel(letters, words, sentences) {
+      if (words === 0 || sentences === 0) {
+        return 0;
+      }
+      let level = Math.round(
+        4.71 * (letters / words) + 0.5 * words / sentences - 21.43
+      );
+      return level <= 0 ? 0 : level;
     },
     async _loadScript(url) {
       return new Promise((resolve, reject) => {
