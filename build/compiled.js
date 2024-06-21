bd95b040f4d7b7bcae0806ae7ce66aa2776f8fd9
Captain Cool <coder0807@gmail.com>
2024-06-21 08:09:35 +0530

    imports test

--- build/compiled.js
+++ build/compiled.js
@@ -1,10 +1,4 @@
 (() => {
-  var __create = Object.create;
-  var __defProp = Object.defineProperty;
-  var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
-  var __getOwnPropNames = Object.getOwnPropertyNames;
-  var __getProtoOf = Object.getPrototypeOf;
-  var __hasOwnProp = Object.prototype.hasOwnProperty;
   var __require = /* @__PURE__ */ ((x) => typeof require !== "undefined" ? require : typeof Proxy !== "undefined" ? new Proxy(x, {
     get: (a, b) => (typeof require !== "undefined" ? require : a)[b]
   }) : x)(function(x) {
@@ -12,25 +6,8 @@
       return require.apply(this, arguments);
     throw new Error('Dynamic require of "' + x + '" is not supported');
   });
-  var __copyProps = (to, from, except, desc) => {
-    if (from && typeof from === "object" || typeof from === "function") {
-      for (let key of __getOwnPropNames(from))
-        if (!__hasOwnProp.call(to, key) && key !== except)
-          __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
-    }
-    return to;
-  };
-  var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
-    // If the importer is in node compatibility mode or this is not an ESM
-    // file that has been converted to a CommonJS file using a Babel-
-    // compatible transform (i.e. "__esModule" has not been set), then set
-    // "default" to the CommonJS "module.exports" for node compatibility.
-    isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target,
-    mod
-  ));
 
   // lib/plugin.js
-  var cheerio = __toESM(__require("cheerio"), 1);
   var import_marked = __require("marked");
   var plugin = {
     constants: {},
@@ -38,13 +15,11 @@
       "Analyze": {
         check: async function(app, noteUUID) {
           const noteContent = await app.getNoteContent({ uuid: noteUUID });
-          const html = import_marked.marked.parse(noteContent);
-          const $ = cheerio.load(html);
-          console.log($.text());
-          return $.text();
+          console.log(import_marked.marked);
+          return noteContent;
         },
         run: async function(app, noteUUID) {
-          await app.alert("You clicked the Baby's first Note Option command in a COOL note!");
+          await app.alert("Hemingway analyzer");
           console.debug("Special message to the DevTools console");
         }
       }
