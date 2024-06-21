ce4696b22cafe594e41fbae720c4a4a549e5cf0c
Captain Cool <coder0807@gmail.com>
2024-06-21 07:33:02 +0530

    marked and cheerio test

--- build/compiled.js
+++ build/compiled.js
@@ -1,30 +1,54 @@
 (() => {
+  var __create = Object.create;
+  var __defProp = Object.defineProperty;
+  var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+  var __getOwnPropNames = Object.getOwnPropertyNames;
+  var __getProtoOf = Object.getPrototypeOf;
+  var __hasOwnProp = Object.prototype.hasOwnProperty;
+  var __require = /* @__PURE__ */ ((x) => typeof require !== "undefined" ? require : typeof Proxy !== "undefined" ? new Proxy(x, {
+    get: (a, b) => (typeof require !== "undefined" ? require : a)[b]
+  }) : x)(function(x) {
+    if (typeof require !== "undefined")
+      return require.apply(this, arguments);
+    throw new Error('Dynamic require of "' + x + '" is not supported');
+  });
+  var __copyProps = (to, from, except, desc) => {
+    if (from && typeof from === "object" || typeof from === "function") {
+      for (let key of __getOwnPropNames(from))
+        if (!__hasOwnProp.call(to, key) && key !== except)
+          __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+    }
+    return to;
+  };
+  var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
+    // If the importer is in node compatibility mode or this is not an ESM
+    // file that has been converted to a CommonJS file using a Babel-
+    // compatible transform (i.e. "__esModule" has not been set), then set
+    // "default" to the CommonJS "module.exports" for node compatibility.
+    isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target,
+    mod
+  ));
+
   // lib/plugin.js
+  var cheerio = __toESM(__require("cheerio"), 1);
+  var import_marked = __require("marked");
   var plugin = {
-    // --------------------------------------------------------------------------------------
     constants: {},
-    // --------------------------------------------------------------------------
-    // https://www.amplenote.com/help/developing_amplenote_plugins#insertText
-    insertText: {},
-    // --------------------------------------------------------------------------
-    // https://www.amplenote.com/help/developing_amplenote_plugins#noteOption
     noteOption: {
-      "Baby's first Note Option command": {
+      "Analyze": {
         check: async function(app, noteUUID) {
           const noteContent = await app.getNoteContent({ uuid: noteUUID });
-          return /cool/i.test(noteContent.toLowerCase());
+          const html = import_marked.marked.parse(noteContent);
+          const $ = cheerio.load(html);
+          console.log($.text());
+          return $.text();
         },
         run: async function(app, noteUUID) {
           await app.alert("You clicked the Baby's first Note Option command in a COOL note!");
           console.debug("Special message to the DevTools console");
         }
       }
-    },
-    // --------------------------------------------------------------------------
-    // https://www.amplenote.com/help/developing_amplenote_plugins#replaceText
-    replaceText: {}
-    // There are several other entry points available, check them out here: https://www.amplenote.com/help/developing_amplenote_plugins#Actions
-    // You can delete any of the insertText/noteOptions/replaceText keys if you don't need them
+    }
   };
   var plugin_default = plugin;
 })();
